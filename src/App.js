2392082867587ad20c8702c64283f35b4d363395
Thiago Pizzutti <tpizzutti@gmail.com>
2020-06-02 16:14:21 -0300

    header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from "react"
+import {Route, Switch } from 'react-router-dom'
 import Header from "./components/Header"
 import Cart from "./pages/Cart"
 import Photos from "./pages/Photos"
 
 function App() {    
     return (
-        <div>
+      <div>
+        <Switch>
             <Header />
+          <Route path='/'>
+            <Photos />
             <h1>Home Page</h1>
+          </Route>
+          <Route path='/cart'>
+            <Cart />
+            
+          </Route>
+        </Switch>
         </div>
+
     )
 }
 
