489f9edd27da2a51b62b720b61ce6df339627ab9
Thiago Pizzutti <tpizzutti@gmail.com>
2020-06-03 15:52:03 -0300

    context heart done

--- src/Context.js
+++ src/Context.js
@@ -1,21 +1,60 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, {
+  createContext,
+  useState,
+  useEffect
+} from 'react';
 import axios from 'axios';
 const Context = createContext();
 
-const ContextProvider = ({ children }) => {
-  const [allPhotos, setAllPhotos] = useState([]);
+const ContextProvider = ({
+    children
+  }) => {
+    const [allPhotos, setAllPhotos] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get(
-        'https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json'
-      )
-      .then((resp) => setAllPhotos(resp.data))
+    const toggleFavorite = (id) => {
+      const isFavoritePhoto = allPhotos.map(favorite => {
 
-      .catch((err) => console.log(err));
-  }, []);
+        if (favorite.id === id) {
+          return {
+            ...allPhotos,
+            isFavorite: !favorite.isFavorite
+          }
+        }
+        return allPhotos
+      })
 
-  return <Context.Provider value={{ allPhotos }}>{children}</Context.Provider>;
-};
+      setAllPhotos(isFavoritePhoto)
 
-export { ContextProvider, Context };
+    }
+
+
+
+
+
+
+
+    useEffect(() => {
+      axios
+        .get(
+          'https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json'
+        )
+        .then((resp) => setAllPhotos(resp.data))
+
+        .catch((err) => console.log(err));
+    }, []);
+
+    return ( <
+      Context.Provider value = {
+        {
+          allPhotos,
+          toggleFavorite
+        }
+      } > {
+        children
+      } <
+      /Context.Provider>);
+    }
+    export {
+      ContextProvider,
+      Context
+    };
--- src/components/Images.js
+++ src/components/Images.js
@@ -1,34 +1,47 @@
-import React, { useState, useCallback} from 'react'
+import React, { useState, useContext, useCallback } from 'react'
+import { Context } from '../Context'
 
 const Images = ({ className, img }) => {
   const [isHovered, setIsHovered] = useState(false)
-
+  
+  const { toggleFavorite } = useContext(Context)
+  
   const handleHoverEnter = useCallback(() => {
   setIsHovered(true)
-    console.log(isHovered);
-  },[isHovered])
+    
+  },[])
   const handleHoverLeave = useCallback(() => {
   setIsHovered(false)
-    console.log(isHovered);
-  },[isHovered])
+   
+  },[])
 
 
   return (
     < div className = {
       `${className} image-container`
     }
+      onMouseEnter={handleHoverEnter}
+      onMouseLeave={handleHoverLeave}
     >
       <img
         src={img.url}
         alt='images'
         className="image-grid"
-        onMouseEnter={handleHoverEnter}
-        onMouseLeave={handleHoverLeave}
+        
       />
       {isHovered && (
         <>
-          <i className="ri-heart-line favorite"></i>
-          <i className="ri-add-circle-line cart"></i>
+          <i
+            className="ri-heart-line favorite"
+            onClick={() => toggleFavorite(img.id)}
+          
+          ></i>
+          <i
+            className="ri-add-circle-line cart"
+            
+          >
+            
+            </i>
         </>
       )}
       
