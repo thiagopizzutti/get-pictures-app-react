8c4f538a85c32f16377bbfb382439f6212914238
Thiago Pizzutti <tpizzutti@gmail.com>
2020-06-04 09:53:07 -0300

    fill cart icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {useContext} from "react";
 import { Link } from 'react-router-dom'
+import {Context} from '../Context'
 
 function Header() {
+  const {cartItems } = useContext(Context);
+  const cartClassName =
+    cartItems.length ? "ri-shopping-cart-fill" : "ri-shopping-cart-line";
+  
+
+
   return (
     <header>
-      <Link to='/'><h2>Pic Some</h2></Link>
-      <Link to= '/cart'><i className="ri-shopping-cart-line ri-fw ri-2x"></i></Link>
+      <h2><Link to='/'>Pic Some</Link></h2>
+     <i className={`${cartClassName} ri-fw ri-2x`}></i>
     </header>
   );
 }
